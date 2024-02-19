3a5500d8a64f160f0a8420e7f242aea576d12297
emmasbox <emmaskovgard@gmail.com>
2024-02-19 21:10:26 +0100

    Added keywords, parameters, string/number recognition, and comments

--- src/jcl.js
+++ src/jcl.js
@@ -8,14 +8,61 @@ export default function (hljs) {
             keyword: [
                 'JOB',
                 'EXEC',
-                'DD'
+                'DD',
+                'OUTPUT',
+                'EXPORT',
+                'INCLUDE',
+                'NOTIFY',
+                'SCHEDULE',
+                'PEND',
+                'SET',
+                'JCLLIB',
+                'ENDCTNL',
+                'XMIT',
+                'PROC',
+
+                'IF',
+                'THEN',
+                'ELSE',
+                'ENDIF'
+            ],
+            built_in: [
+                'DSN',
+                'ZFSDSN',
+                'PGM',
+                'MSGLEVEL',
+                'MSGCLASS',
+                'REGION',
+                'COND',
+                'CCSID',
+                'ACCT',
+                'PARMDD',
+                'DYNAMNBR',
+                'PERFORM',
+                'MEMLIMIT',
+                'TIME',
+                'RD',
+                'PROC',
+                'SYSOUT',
+                'SYSIN'
             ],
             meta: [
-
+                '//',
+                '/*'
             ]
         },
         contains: [
-
+            hljs.COMMENT(
+                /\/\/\*.*/
+            ),
+            {
+                scope: 'string',
+                begin: "'", end: "'"
+            },
+            {
+                scope: 'number',
+                begin: /\s([0-9]+(?:(\.)[0-9]+)*)/
+            },
         ]
     }
 }
