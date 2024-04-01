c8350f8f4cd242f9533119854edd9029e76256e5
emmasbox <emmaskovgard@gmail.com>
2024-04-01 20:19:38 +0200

    Added ability to recognize step names

--- src/jcl.js
+++ src/jcl.js
@@ -156,7 +156,7 @@ export default function (hljs) {
             },
             {
                 scope: 'meta',
-                begin: /(\/\/|\/\*)/
+                begin: /(\/\/\w+|\/\*)/
             }
         ]
     }
